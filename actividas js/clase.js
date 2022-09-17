ab963d4d829d6277babc3bd8a33616f65d1c2636
orlansx9 <72472623+orlansx9@users.noreply.github.com>
2022-09-17 01:36:01 -0500

    Add files via upload
    actividad de js

---
+++ actividas js/clase.js
@@ -0,0 +1,23 @@
+function despedir(){
+    alert("adios desde un archivo");
+    
+}
+
+function mostrar(){
+    alert("Profe va muy rapido");
+}
+
+function otroclick(){
+    alert("escribir bien");
+}
+
+function presionar(){
+    alert("ya tengo hambre");
+}
+console.log("hola desde la consola");
+console.log(20);
+console.log(2.6);
+console.log(1,63);
+console.log(1+2);
+console.log(true);
+console.log(false);
